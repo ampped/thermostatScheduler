72f00a2ac587896ed8b618791205e40bd5083a22
Amy Pham <amp4129@g.rit.edu>
2018-03-29 09:46:33 -0400

    change weather

--- app/js/weather.js
+++ app/js/weather.js
@@ -4,13 +4,14 @@ var weather;
 var forecast;
 
 function getWeatherData(zip){
-	var url =  'https://api.apixu.com/v1/forecast.json?key=8345029550ea4066b4a03622172106&q=94089&days=7';
+	//var url =  'https://api.apixu.com/v1/forecast.json?key=7f465411919341c393d73815182903&q=14623&days=7';
+	var url =  'http://api.aerisapi.com/forecasts/rochester,ny?filter=1hr&limit=999&client_id=btq5TOAD6ArvUjPYuyebw&client_secret=I4hUZsjsJKEvzZNM7ZC8tqAoqesCjwY0EzaQ0a78';
 	$.ajax({
 	  dataType: "json",
 	  url: url,
 	  data: null,
 	  success: function(obj){
-	  	weather = obj.forecast.forecastday;
+	  	weather = obj.response[0].periods;
 	  	app.main.init();
 	  },
 	  error: function(){
